d274986176d29c14aaa663f7d0ba585013f8ca49
Kozionov <e-kozionov@mail.ru>
2020-12-18 16:13:23 +1200

    Задание №3. Создать возрастающий массив из 100 чисел.

--- Lesson2_Base_operators/main.swift
+++ Lesson2_Base_operators/main.swift
@@ -7,22 +7,11 @@
 
 import Foundation
 
-//Задание №2
-//Написать функцию, которая определяет, делится ли число без остатка на 3.
+//Задание №3
+//Создать возрастающий массив из 100 чисел.
 
-func devThree(numIn: Int){
-    let res: Double = round(Double(numIn) / 3 * 1000) / 1000
-    if numIn % 3 == 0 {
-        print("Число \(numIn) делится на 3 без остатка")
-    } else {
-        print("Число \(numIn) делится на 3 с остатком и равно \(res)")
-    }
-    
-}
+var arrNum = Array(1...100)
 
-devThree(numIn: 9)
-devThree(numIn: 67)
-devThree(numIn: 109)
-devThree(numIn: 88)
+arrNum.forEach({print($0)})
 
 
