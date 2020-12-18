c8da3b0905d42b8fe1c90edb7e653fd2c170e775
Kozionov <e-kozionov@mail.ru>
2020-12-18 15:50:38 +1200

    Задание №2. Написать функцию, которая определяет, делится ли число без остатка на 3.

--- Lesson2_Base_operators/main.swift
+++ Lesson2_Base_operators/main.swift
@@ -7,18 +7,22 @@
 
 import Foundation
 
-//Задание №1
-//Написать функцию, которая определяет, четное число или нет.
+//Задание №2
+//Написать функцию, которая определяет, делится ли число без остатка на 3.
 
-func typeNum(num: Int){
-    if  num % 2 == 0 {
-        print("Число \(num) чётное")
+func devThree(numIn: Int){
+    let res: Double = round(Double(numIn) / 3 * 1000) / 1000
+    if numIn % 3 == 0 {
+        print("Число \(numIn) делится на 3 без остатка")
     } else {
-        print("Число \(num) не чётное")
+        print("Число \(numIn) делится на 3 с остатком и равно \(res)")
     }
     
 }
 
-typeNum(num: 8)
+devThree(numIn: 9)
+devThree(numIn: 67)
+devThree(numIn: 109)
+devThree(numIn: 88)
 
 
